443d4449799a872a1d1ab8ba67a3d14b15768ac8
Luis Gomez <lfgomez@inmetrics>
2018-06-23 14:22:50 -0300

    REST Code example for a Presence Sensor

---
+++ PresenceSensor_REST/PresenceSensor_REST.ino
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h> 
+#include <ESP8266HTTPClient.h>
+
+WiFiClient espClient;
+HTTPClient http;
+
+//Dados de Wifi
+const char* WIFI_SSID = "";
+const char* WIFI_PASS = "";
+
+// Dados do servidor
+const char* USER = "";
+const char* PWD = "";
+const char* PUB_pir = "";
+
+//GPIO usado para o sensor de presenca
+int presence_gpio = 4;
+
+//Variaveis gloabais desse codigo
+char bufferJ[256];
+int httpCode = 0;
+int mov=0;
+
+char *jsonMQTTmsgDATAint(const char *device_id, const char *metric, int value)
+  {
+      StaticJsonBuffer<200> jsonMQTT;
+      JsonObject& jsonMSG = jsonMQTT.createObject();
+        jsonMSG["name"] = device_id;
+        jsonMSG["metric"] = metric;
+        jsonMSG["value"] = value;
+        jsonMSG.printTo(bufferJ, sizeof(bufferJ));
+        return bufferJ;
+  }
+  
+void handleInterrupt4() {
+  //Serial.println("Interrupt GPIO4 !!!");
+  mov++;
+}
+  
+void check_connection() 
+{
+  int i =0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    i++;
+    Serial.print(".");
+    if (i > 100) ESP.deepSleep(300*1000000, WAKE_RF_DEFAULT); //Tentar conectar por 50 segundos, caso nao consiga, dormir por 5 minutos
+  }
+}
+
+void setup()
+{
+  pinMode(presence_gpio, INPUT);
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID,WIFI_PASS);
+  check_connection();
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Accept", "application/json");
+  http.setAuthorization(USER, PWD);
+}
+
+
+
+void loop()
+{
+  WiFi.forceSleepBegin(60000000);
+  delay(1);
+  attachInterrupt(digitalPinToInterrupt(presence_gpio), handleInterrupt4, RISING);
+  delay(60000);
+  detachInterrupt(digitalPinToInterrupt(presence_gpio));
+  WiFi.forceSleepWake();
+  delay(1);
+  Serial.println("Sending data!");
+  check_connection();
+  http.begin("data.demo.konkerlabs.net",80,PUB_pir);
+  httpCode=http.POST(jsonMQTTmsgDATAint("movement_detector", "Number", mov));
+  Serial.println(httpCode);
+  http.end();
+  mov=0;
+}
+
